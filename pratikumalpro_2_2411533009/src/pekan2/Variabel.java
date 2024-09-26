fc5be3e52dbe8a054b93a42e20e0098f4347e4f5
HP <HP@FTI-LAB03-PC07>
2024-09-26 11:00:25 +0700

    pratikum pekan 2

---
+++ pratikumalpro_2_2411533009/src/pekan2/Variabel.java
@@ -0,0 +1,13 @@
+package pekan2;
+
+public class Variabel {
+
+	public static void main(String[] args) {
+	int x;
+	x = 3;
+	System.out.println("x adalah " + x);// x is 3
+	System.out.println(5 * x - 1);// 5 * 3 - 1
+
+	}
+
+}
